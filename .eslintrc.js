ae8c6f3f4247e68d043d1e52f8bf029089deaef0
Jonas <jonaslagoteixeira@gmail.com>
2019-12-02 19:13:51 -0300

    configurado o json-server

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 0,
+    'jsx-a11y/control-has-associated-label': 0,
   },
 };
