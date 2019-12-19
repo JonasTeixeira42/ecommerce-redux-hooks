c1724b072973a3b78d75a0de085e107cf6d0eed5
Jonas <jonaslagoteixeira@gmail.com>
2019-12-19 13:44:23 -0300

    feito a conversao da classe home de redux para hooks

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
